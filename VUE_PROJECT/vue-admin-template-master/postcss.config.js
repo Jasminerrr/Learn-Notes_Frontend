e077a375606aee584670306a754d35db5e83df2e
Jasminerrr <jasminerrr_zhm@163.com>
2022-12-07 15:51:03 +0800

    新增后台管理系统文件

---
+++ VUE_PROJECT/vue-admin-template-master/postcss.config.js
@@ -0,0 +1,9 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+// 跟css相关的配置文件
+module.exports = {
+  'plugins': {
+    // to edit target browsers: use "browserslist" field in package.json
+    // 在不同浏览器前面可以加前缀---解决兼容问题
+    'autoprefixer': {}
+  }
+}
